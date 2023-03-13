45fde6c8e23a5ee30100d3f3c533478b7f14dfcf
vigneshvicky1234 <127626606+vigneshvicky1234@users.noreply.github.com>
2023-03-13 06:38:38 +0530

    Add files via upload

---
+++ Fibonacci Series.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a = 0;
+int b = 1;
+int i = 0;
+while (i <= 5)
+{
+    int c = a + b;
+    a = b;
+    b = c;
+    i++;
+    Console.Write(c + " ");
+}
+
